ef296ba275bc6251557c6173b72c28baa113922a
ambroan1 <112683392+ambroan1@users.noreply.github.com>
2022-10-07 14:32:04 +0200

    I commit query bed

--- src/query_bed.py
+++ src/query_bed.py
@@ -37,7 +37,7 @@ def main() -> None:
         for feature in table.get_chrom(lst[0]):
             if feature.chrom_start >= int(lst[1]) and feature.chrom_end < int(lst[2]):
                 print_line(feature, args.outfile)    
-    args.outfile.close()
+    #args.outfile.close()
 
 
 if __name__ == '__main__':
